d34d9d716e3efc47bd5a90d9440248bd7174c9a3
Mohsen Jalali <mniroomandj@gmail.com>
2022-12-09 18:38:04 +0330

    fix: override == operator

--- lib/src/model/calendar_date_time.dart
+++ lib/src/model/calendar_date_time.dart
@@ -4,45 +4,47 @@ enum CalendarType { jalali, gregorian }
 
 enum CalendarMode { weekly, monthlyTable , monthlyLinear }
 
-enum CalendarDateType { previousMonth, nextMonth, current }
 
 class CalendarDateTime {
   int year;
   int month;
   int day;
   CalendarType calendarType;
-  CalendarDateType dateType;
 
   CalendarDateTime(
     this.year,
     this.month,
     this.day, {
     this.calendarType = CalendarType.gregorian,
-    this.dateType = CalendarDateType.current,
   });
 
-  factory CalendarDateTime.fromDateTime(DateTime dateTime,
-          {CalendarDateType dateType = CalendarDateType.current}) =>
+  factory CalendarDateTime.fromDateTime(DateTime dateTime) =>
       CalendarDateTime(
         dateTime.year,
         dateTime.month,
         dateTime.day,
         calendarType: CalendarType.jalali,
-        dateType: dateType,
       );
 
-  factory CalendarDateTime.fromJalali(Jalali jalali,
-          {CalendarDateType dateType = CalendarDateType.current}) =>
+  factory CalendarDateTime.fromJalali(Jalali jalali) =>
       CalendarDateTime(
         jalali.year,
         jalali.month,
         jalali.day,
         calendarType: CalendarType.jalali,
-        dateType: dateType,
       );
 
   @override
   String toString() {
     return "$year-$month-$day";
   }
+
+  @override
+  bool operator ==(Object other) {
+    if(other is CalendarDateTime){
+      return year == other.year && month == other.month && day == other.day;
+    }
+    return super == other;
+  }
+
 }
