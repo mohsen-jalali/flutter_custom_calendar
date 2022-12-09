c93d1e79e9ce436f134a1cf01a5f68724ae2da59
Mohsen Jalali <mniroomandj@gmail.com>
2022-12-09 18:49:13 +0330

    fix: move calendar week day title

--- lib/src/widgets/calendar_monthly_widget.dart
+++ lib/src/widgets/calendar_monthly_widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_calendar/flutter_custom_calendar.dart';
 import 'package:flutter_custom_calendar/src/provider/calendar_provider.dart';
-import 'package:flutter_custom_calendar/src/utils/calendar_date_time_extension.dart';
 import 'package:flutter_custom_calendar/src/widgets/calendar_day_widget.dart';
+import 'package:flutter_custom_calendar/src/widgets/calendar_week_day_row.dart';
 import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:provider/provider.dart';
 
@@ -12,8 +12,6 @@ class CalendarMonthlyWidget extends StatelessWidget {
   final Function(int) onMonthChanged;
   final Function(CalendarDateTime) onSelectDate;
   final CalendarDayModel? calendarDayModel;
-
-
 
   const CalendarMonthlyWidget({
     Key? key,
@@ -26,45 +24,55 @@ class CalendarMonthlyWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      height: calculateHeight,
-      width: MediaQuery.of(context).size.width,
-      duration: const Duration(milliseconds: 300),
-      child: PageView.builder(
-        controller: pageController,
-        onPageChanged: onMonthChanged,
-        itemBuilder: (context, index) => GridView.builder(
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 7,
-            mainAxisExtent: 50,
-          ),
-          physics: const NeverScrollableScrollPhysics(),
-          itemCount: calendarDates.length,
-          itemBuilder: (context, index) {
-            return AnimationConfiguration.staggeredGrid(
-              columnCount: 7,
-              position: index,
-              duration: const Duration(milliseconds: 400),
-              child: ScaleAnimation(
-                child: FadeInAnimation(
-                  child: CalendarDayWidget(
-                    calendarDateTime: calendarDates[index],
-                    calendarDateModel: calendarDayModel,
-                    isSelected: context.read<CalendarProvider>().selectedDate ==
-                        calendarDates[index],
-                    isOverFlow:
-                        context.read<CalendarProvider>().currentTime.month !=
-                            calendarDates[index].month,
-                    onSelectDate: () {
-                      onSelectDate.call(calendarDates[index]);
-                    },
-                  ),
-                ),
-              ),
-            );
-          },
+    return Column(
+      children: [
+        RowCalendarWeekDayTitle(
+          calendarType: context.read<CalendarProvider>().calendarType,
         ),
-      ),
+        AnimatedContainer(
+          height: calculateHeight,
+          width: MediaQuery.of(context).size.width,
+          duration: const Duration(milliseconds: 300),
+          child: PageView.builder(
+            controller: pageController,
+            onPageChanged: onMonthChanged,
+            itemBuilder: (context, index) => GridView.builder(
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 7,
+                mainAxisExtent: 50,
+              ),
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: calendarDates.length,
+              itemBuilder: (context, index) {
+                return AnimationConfiguration.staggeredGrid(
+                  columnCount: 7,
+                  position: index,
+                  duration: const Duration(milliseconds: 400),
+                  child: ScaleAnimation(
+                    child: FadeInAnimation(
+                      child: CalendarDayWidget(
+                        calendarDateTime: calendarDates[index],
+                        calendarDateModel: calendarDayModel,
+                        isSelected:
+                            context.read<CalendarProvider>().selectedDate ==
+                                calendarDates[index],
+                        isOverFlow: context
+                                .read<CalendarProvider>()
+                                .currentTime
+                                .month !=
+                            calendarDates[index].month,
+                        onSelectDate: () {
+                          onSelectDate.call(calendarDates[index]);
+                        },
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 
