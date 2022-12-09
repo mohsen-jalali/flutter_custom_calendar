08b4149406a0a8bcb499c27ec06dfab6440dc69b
Mohsen Jalali <mniroomandj@gmail.com>
2022-12-09 18:51:19 +0330

    fix: convert to stateless

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,17 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_custom_calendar/src/model/calendar_date_time.dart';
 import 'package:flutter_custom_calendar/src/model/custom_day_model.dart';
+import 'package:flutter_custom_calendar/src/model/custom_header_model.dart';
 import 'package:flutter_custom_calendar/src/provider/calendar_provider.dart';
+import 'package:flutter_custom_calendar/src/widgets/calendar_header.dart';
 import 'package:flutter_custom_calendar/src/widgets/calendar_monthly_widget.dart';
 import 'package:flutter_custom_calendar/src/widgets/calendar_week_day_row.dart';
 import 'package:provider/provider.dart';
 
-class CustomCalendar extends StatefulWidget {
+class CustomCalendar extends StatelessWidget {
   final CalendarType calendarType;
   final bool includeDisableDays;
   final CalendarDateTime? selectedDate;
   final Function(CalendarDateTime)? onSelectDate;
   final CalendarDayModel? calendarDayModel;
+  final HeaderModel? headerModel;
   final Color? backgroundColor;
   final Decoration? backgroundDecoration;
   final EdgeInsets? padding;
@@ -26,82 +29,61 @@ class CustomCalendar extends StatefulWidget {
     this.backgroundColor,
     this.backgroundDecoration,
     this.padding,
+    this.headerModel,
   }) : super(key: key);
 
   @override
-  State<CustomCalendar> createState() => _CustomCalendarState();
-}
-
-class _CustomCalendarState extends State<CustomCalendar> {
-  PageController pageController = PageController(initialPage: 1000);
-  int currentPage = 1000;
-
-  @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (_) => CalendarProvider(
-            calendarType: widget.calendarType,
-          ),
-        ),
-      ],
-      builder: (context, child) => Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 16),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                IconButton(
-                  onPressed: () {
-                    context.read<CalendarProvider>().increaseMonth();
-                    currentPage -= 1;
-                    pageController.previousPage(
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeInOut);
-                  },
-                  icon: const Icon(Icons.arrow_back),
-                ),
-                Expanded(
-                  child: Center(
-                    child: Text(context
-                        .watch<CalendarProvider>()
-                        .currentTime
-                        .toString()),
-                  ),
-                ),
-                IconButton(
-                  onPressed: () {
-                    context.read<CalendarProvider>().decreaseMonth();
-                    currentPage += 1;
-                    pageController.nextPage(
-                        duration: const Duration(milliseconds: 300),
-                        curve: Curves.easeInOut);
-                  },
-                  icon: const Icon(Icons.arrow_forward),
-                ),
-              ],
-            ),
-            RowCalendarWeekDayTitle(
-              calendarType: widget.calendarType,
-            ),
-            Consumer<CalendarProvider>(
-              builder: (context, provider, child) => CalendarMonthlyWidget(
-                pageController: pageController,
-                onMonthChanged: (index) {
-                  if (currentPage > index) {
-                    context.read<CalendarProvider>().increaseMonth();
-                  }
-                  if (currentPage < index) {
-                    context.read<CalendarProvider>().decreaseMonth();
-                  }
-                  currentPage = index;
-                },
-                calendarDayModel: widget.calendarDayModel,
-                calendarDates: provider.calendarDates,
+    return Material(
+      color: Colors.transparent,
+      child: Container(
+        padding: padding,
+        clipBehavior: Clip.antiAlias,
+        decoration: backgroundDecoration ??
+            BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    spreadRadius: 0,
+                    blurRadius: 10,
+                  )
+                ]),
+        child: MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+              create: (_) => CalendarProvider(
+                calendarType: calendarType,
               ),
             ),
           ],
+          builder: (context, child) => Column(
+            children: [
+              CalendarHeader(
+                calendarDateTime: context.watch<CalendarProvider>().currentTime,
+                onPressNext: () {
+                  context.read<CalendarProvider>().goToNextPage();
+                },
+                onPressPrevious: () {
+                  context.read<CalendarProvider>().goToPreviousPage();
+                },
+                headerModel: headerModel,
+              ),
+              Consumer<CalendarProvider>(
+                builder: (context, provider, child) => CalendarMonthlyWidget(
+                  pageController: provider.pageController,
+                  onMonthChanged: provider.onChangeMonthPageView,
+                  onSelectDate: (calendarDateTime) {
+                    provider.selectDate(calendarDateTime);
+                    onSelectDate?.call(calendarDateTime);
+                  },
+                  calendarDayModel: calendarDayModel,
+                  calendarDates: provider.calendarDates,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
