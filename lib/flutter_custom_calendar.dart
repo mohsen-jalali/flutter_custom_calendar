a73a28206c6e93bc3b161fb999656983127647df
Mohsen Jalali <mniroomandj@gmail.com>
2022-12-09 18:50:50 +0330

    fix: export  header model

--- lib/flutter_custom_calendar.dart
+++ lib/flutter_custom_calendar.dart
@@ -4,3 +4,4 @@ library flutter_custom_calendar;
 export 'src/main.dart';
 export 'src/model/calendar_date_time.dart';
 export 'src/model/custom_day_model.dart';
+export 'src/model/custom_header_model.dart';
