693180d0729817d0c08567ca87b4adad77980c67
Mohsen Jalali <mniroomandj@gmail.com>
2022-12-15 21:09:26 +0330

    fix: add initial function

--- lib/src/provider/calendar_provider.dart
+++ lib/src/provider/calendar_provider.dart
@@ -5,82 +5,97 @@ import 'package:flutter_custom_calendar/src/utils/jalali_extension.dart';
 import 'package:shamsi_date/shamsi_date.dart';
 
 class CalendarProvider extends ChangeNotifier {
-  final CalendarType calendarType;
-
-  CalendarProvider({
-    required this.calendarType,
-  }) {
-    if (calendarType == CalendarType.jalali) {
-      currentTime = CalendarDateTime.fromJalali(Jalali.now());
-    } else {
-      currentTime = CalendarDateTime.fromDateTime(DateTime.now());
-    }
-    generateCalendarDateTimeList();
-  }
-
-  late CalendarDateTime currentTime;
+  CalendarType calendarType = CalendarType.gregorian;
+  CalendarDateTime calendarDate = CalendarDateTime.fromDateTime(DateTime.now());
   CalendarDateTime? selectedDate;
   List<CalendarDateTime> calendarDates = [];
-  PageController pageController = PageController(initialPage: 1000);
-  int currentPage = 1000;
+  PageController pageController = PageController(initialPage: 100000);
+
+  void initialCalendar(
+      CalendarType calendarType, CalendarDateTime? initialDate) {
+    this.calendarType = calendarType;
+    if (initialDate != null) {
+      calendarDate = initialDate;
+    } else {
+      if (calendarType == CalendarType.jalali) {
+        calendarDate = CalendarDateTime.fromJalali(Jalali.now());
+      } else {
+        calendarDate = CalendarDateTime.fromDateTime(DateTime.now());
+      }
+    }
+    _generateCalendarDateTimeList();
+    selectDate(calendarDate);
+  }
+
+  void goToCurrentDate() {
+    if (calendarType == CalendarType.jalali) {
+      calendarDate = CalendarDateTime.fromJalali(Jalali.now());
+    } else {
+      calendarDate = CalendarDateTime.fromDateTime(DateTime.now());
+    }
+    _generateCalendarDateTimeList();
+    selectDate(calendarDate);
+  }
 
   void goToNextPage() {
     pageController.nextPage(
         duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
+    _increaseMonth();
   }
 
   void goToPreviousPage() {
     pageController.previousPage(
         duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
-  }
-
-  void onChangeMonthPageView(int index) {
-    if (currentPage > index) {
-      decreaseMonth();
-    }
-    if (currentPage < index) {
-      increaseMonth();
-    }
-    currentPage = index;
+    _decreaseMonth();
   }
 
   void selectDate(CalendarDateTime date) {
     selectedDate = date;
     notifyListeners();
-    if (currentTime.month != date.month) {
-      if (currentTime.isAfter(date) == 1) {
+    if (calendarDate.month != date.month) {
+      if (calendarDate.isAfter(date) == 1) {
         goToPreviousPage();
-      } else if (currentTime.isAfter(date) == -1) {
+      } else if (calendarDate.isAfter(date) == -1) {
         goToNextPage();
       }
     }
   }
 
-  void increaseMonth() {
-    currentTime = currentTime.increaseMonth;
-    generateCalendarDateTimeList();
+  void changeCalendarType(CalendarType calendarType) {
+    if (this.calendarType == calendarType) return;
+    this.calendarType = calendarType;
+    calendarDate = calendarDate.changeCalendarType(calendarType);
+    if (selectedDate != null) {
+      selectedDate = selectedDate!.changeCalendarType(calendarType);
+    }
+    _generateCalendarDateTimeList();
   }
 
-  void decreaseMonth() {
-    currentTime = currentTime.decreaseMonth;
-    generateCalendarDateTimeList();
+  void _increaseMonth() {
+    calendarDate = calendarDate.increaseMonth;
+    _generateCalendarDateTimeList();
   }
 
-  void generateCalendarDateTimeList() {
+  void _decreaseMonth() {
+    calendarDate = calendarDate.decreaseMonth;
+    _generateCalendarDateTimeList();
+  }
+
+  void _generateCalendarDateTimeList() {
     calendarDates.clear();
     switch (calendarType) {
       case CalendarType.jalali:
-        calculateJalaliDateTimes();
+        _calculateJalaliDateTimes();
         break;
       case CalendarType.gregorian:
-        calculateGregorianDateTimes();
+        _calculateGregorianDateTimes();
         break;
     }
     notifyListeners();
   }
 
-  void calculateJalaliDateTimes() {
-    Jalali jalali = Jalali(currentTime.year, currentTime.month, 1);
+  void _calculateJalaliDateTimes() {
+    Jalali jalali = Jalali(calendarDate.year, calendarDate.month, 1);
     for (int index = jalali.weekDay - 1; index >= 1; index--) {
       calendarDates.add(
         CalendarDateTime.fromJalali(
@@ -88,7 +103,7 @@ class CalendarProvider extends ChangeNotifier {
         ),
       );
     }
-    while (jalali.month == currentTime.month) {
+    while (jalali.month == calendarDate.month) {
       calendarDates.add(CalendarDateTime.fromJalali(
         jalali,
       ));
@@ -104,20 +119,20 @@ class CalendarProvider extends ChangeNotifier {
     }
   }
 
-  void calculateGregorianDateTimes() {
-    DateTime dateTime = DateTime(currentTime.year, currentTime.month, 1);
+  void _calculateGregorianDateTimes() {
+    DateTime dateTime = DateTime(calendarDate.year, calendarDate.month, 1);
     for (int index = dateTime.weekday; index > 1; index--) {
       calendarDates.add(CalendarDateTime.fromDateTime(
         dateTime.subtract(Duration(days: index - 1)),
       ));
     }
-    while (dateTime.month == currentTime.month) {
+    while (dateTime.month == calendarDate.month) {
       calendarDates.add(CalendarDateTime.fromDateTime(
         dateTime,
       ));
 
       dateTime =
-          DateTime(currentTime.year, currentTime.month, dateTime.day + 1);
+          DateTime(calendarDate.year, calendarDate.month, dateTime.day + 1);
     }
     if (dateTime.weekday > 1) {
       for (int index = dateTime.weekday; index <= 7; index++) {
