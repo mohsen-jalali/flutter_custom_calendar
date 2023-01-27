76b3709a74887a229f1ea39156b6d6a1750325f5
Mohsen Jalali <mniroomandj@gmail.com>
2023-01-27 10:37:43 +0330

    fix: make calendar provider abstract

--- lib/src/provider/calendar_provider.dart
+++ lib/src/provider/calendar_provider.dart
@@ -1,146 +1,50 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_custom_calendar/src/model/calendar_date_time.dart';
+import 'package:flutter_custom_calendar/flutter_custom_calendar.dart';
+import 'package:flutter_custom_calendar/src/provider/gregorian_calendar_provider.dart';
+import 'package:flutter_custom_calendar/src/provider/jalali_calendar_provider.dart';
 import 'package:flutter_custom_calendar/src/utils/calendar_date_time_extension.dart';
-import 'package:flutter_custom_calendar/src/utils/jalali_extension.dart';
-import 'package:shamsi_date/shamsi_date.dart';
+import 'package:scoped_model/scoped_model.dart';
 
-class CalendarProvider extends ChangeNotifier {
-  CalendarType calendarType = CalendarType.gregorian;
-  CalendarDateTime calendarDate = CalendarDateTime.fromDateTime(DateTime.now());
-  CalendarDateTime? selectedDate;
-  List<CalendarDateTime> calendarDates = [];
-  PageController pageController = PageController(initialPage: 100000);
+abstract class CalendarProvider extends Model {
+  CalendarType calendarType;
+  CalendarDateTime selectedDate;
 
-  void initialCalendar(
-      CalendarType calendarType, CalendarDateTime? initialDate) {
-    this.calendarType = calendarType;
-    if (initialDate != null) {
-      calendarDate = initialDate;
-    } else {
-      if (calendarType == CalendarType.jalali) {
-        calendarDate = CalendarDateTime.fromJalali(Jalali.now());
-      } else {
-        calendarDate = CalendarDateTime.fromDateTime(DateTime.now());
-      }
-    }
-    _generateCalendarDateTimeList();
-    selectDate(calendarDate);
+  CalendarProvider({
+    required this.calendarType,
+    required this.selectedDate,
+  }) {
+    initCalendarDateTime();
   }
 
-  void goToCurrentDate() {
-    if (calendarType == CalendarType.jalali) {
-      calendarDate = CalendarDateTime.fromJalali(Jalali.now());
-    } else {
-      calendarDate = CalendarDateTime.fromDateTime(DateTime.now());
-    }
-    _generateCalendarDateTimeList();
-    selectDate(calendarDate);
-  }
+  late CalendarDateTime calendarDateTime;
 
-  void goToNextPage() {
-    pageController.nextPage(
-        duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
-    _increaseMonth();
-  }
-
-  void goToPreviousPage() {
-    pageController.previousPage(
-        duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
-    _decreaseMonth();
-  }
-
-  void selectDate(CalendarDateTime date) {
-    selectedDate = date;
-    notifyListeners();
-    if (calendarDate.month != date.month) {
-      if (calendarDate.isAfter(date) == 1) {
-        goToPreviousPage();
-      } else if (calendarDate.isAfter(date) == -1) {
-        goToNextPage();
-      }
-    }
-  }
-
-  void changeCalendarType(CalendarType calendarType) {
-    if (this.calendarType == calendarType) return;
-    this.calendarType = calendarType;
-    calendarDate = calendarDate.changeCalendarType(calendarType);
-    if (selectedDate != null) {
-      selectedDate = selectedDate!.changeCalendarType(calendarType);
-    }
-    _generateCalendarDateTimeList();
-  }
-
-  void _increaseMonth() {
-    calendarDate = calendarDate.increaseMonth;
-    _generateCalendarDateTimeList();
-  }
-
-  void _decreaseMonth() {
-    calendarDate = calendarDate.decreaseMonth;
-    _generateCalendarDateTimeList();
-  }
-
-  void _generateCalendarDateTimeList() {
-    calendarDates.clear();
-    switch (calendarType) {
-      case CalendarType.jalali:
-        _calculateJalaliDateTimes();
-        break;
-      case CalendarType.gregorian:
-        _calculateGregorianDateTimes();
-        break;
-    }
-    notifyListeners();
-  }
-
-  void _calculateJalaliDateTimes() {
-    Jalali jalali = Jalali(calendarDate.year, calendarDate.month, 1);
-    for (int index = jalali.weekDay - 1; index >= 1; index--) {
-      calendarDates.add(
-        CalendarDateTime.fromJalali(
-          jalali.subtract(Duration(days: index)),
-        ),
+  factory CalendarProvider.createInstance({
+    required CalendarType calendarType,
+    required CalendarDateTime? selectedDate,
+  }) {
+    if (calendarType == CalendarType.gregorian) {
+      return GregorianCalendarProvider(
+        selectedDate: selectedDate,
       );
     }
-    while (jalali.month == calendarDate.month) {
-      calendarDates.add(CalendarDateTime.fromJalali(
-        jalali,
-      ));
-      jalali = jalali.addDays(1);
-    }
-    if (jalali.weekDay > 1) {
-      for (int index = jalali.weekDay; index <= 7; index++) {
-        calendarDates.add(CalendarDateTime.fromJalali(
-          jalali,
-        ));
-        jalali = jalali.addDays(1);
-      }
-    }
+    return JalaliCalendarProvider(selectedDate: selectedDate);
   }
 
-  void _calculateGregorianDateTimes() {
-    DateTime dateTime = DateTime(calendarDate.year, calendarDate.month, 1);
-    for (int index = dateTime.weekday; index > 1; index--) {
-      calendarDates.add(CalendarDateTime.fromDateTime(
-        dateTime.subtract(Duration(days: index - 1)),
-      ));
-    }
-    while (dateTime.month == calendarDate.month) {
-      calendarDates.add(CalendarDateTime.fromDateTime(
-        dateTime,
-      ));
+  void initCalendarDateTime();
 
-      dateTime =
-          DateTime(calendarDate.year, calendarDate.month, dateTime.day + 1);
-    }
-    if (dateTime.weekday > 1) {
-      for (int index = dateTime.weekday; index <= 7; index++) {
-        calendarDates.add(CalendarDateTime.fromDateTime(
-          dateTime,
-        ));
-        dateTime = dateTime.add(const Duration(days: 1));
-      }
-    }
+  List<CalendarDateTime> getMonthDateLit();
+
+  void selectCurrentDate();
+
+  void increaseMonth() {
+    calendarDateTime = calendarDateTime.increaseMonth;
+  }
+
+  void decreaseMonth() {
+    calendarDateTime = calendarDateTime.decreaseMonth;
+  }
+
+  void selectCalendarDate(CalendarDateTime selectedDate) {
+    this.selectedDate = selectedDate;
   }
 }
