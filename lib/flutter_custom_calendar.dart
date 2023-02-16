0b0131b3a9ccae377510f24697dbf048caa07025
Mohsen Jalali <mniroomandj@gmail.com>
2023-02-16 16:01:44 +0330

    feat: export range picker widget

--- lib/flutter_custom_calendar.dart
+++ lib/flutter_custom_calendar.dart
@@ -1,7 +1,10 @@
 library flutter_custom_calendar;
 
 
-export 'src/main.dart';
+export 'src/calendar.dart';
+export 'src/calendar_range_picker.dart';
 export 'src/model/calendar_date_time.dart';
 export 'src/model/custom_day_model.dart';
 export 'src/model/custom_header_model.dart';
+export 'src/model/picked_range_model.dart';
+export 'src/model/custom_range_day_model.dart';
