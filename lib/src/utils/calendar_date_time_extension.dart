bb1713c5a9f945e8eb5ef3fe54196ca26ae81713
Mohsen Jalali <mniroomandj@gmail.com>
2023-02-16 15:59:07 +0330

    fix: check calendarType in toJalali & toDateTime methods

--- lib/src/utils/calendar_date_time_extension.dart
+++ lib/src/utils/calendar_date_time_extension.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_custom_calendar/flutter_custom_calendar.dart';
+import 'package:flutter_custom_calendar/src/utils/jalali_extension.dart';
 import 'package:shamsi_date/shamsi_date.dart';
 
 extension CalendarDateTimeExtension on CalendarDateTime {
@@ -10,48 +11,57 @@ extension CalendarDateTimeExtension on CalendarDateTime {
     return CalendarDateTime(year - 1, 12, 1, calendarType: calendarType);
   }
 
-  CalendarDateTime changeCalendarType(CalendarType calendarType){
-    if(calendarType == CalendarType.jalali){
+  CalendarDateTime changeCalendarType(CalendarType calendarType) {
+    if (calendarType == CalendarType.jalali) {
       return CalendarDateTime.fromJalali(toDateTime.toJalali());
     }
     return CalendarDateTime.fromDateTime(toJalali.toDateTime());
   }
 
-  bool get isBeforeNow{
-    if(calendarType == CalendarType.jalali){
+  bool get isBeforeNow {
+    if (calendarType == CalendarType.jalali) {
       return CalendarDateTime.fromJalali(Jalali.now()).isAfter(this) == 1;
     }
-    return CalendarDateTime.fromDateTime(DateTime.now()).isAfter(this)  == 1;
+    return CalendarDateTime.fromDateTime(DateTime.now()).isAfter(this) == 1;
   }
 
-
-  Jalali get toJalali{
-    return Jalali(year,month,day);
+  int differenceInDays(CalendarDateTime date) {
+    return date.toDateTime.difference(toDateTime).inDays.abs();
   }
 
-  DateTime get toDateTime{
-    return DateTime(year,month,day);
+  Jalali get toJalali {
+    if(calendarType == CalendarType.gregorian){
+      return toDateTime.toJalali();
+    }
+    return Jalali(year, month, day);
   }
 
-  int isAfter(CalendarDateTime dateTime){
-    if(year > dateTime.year){
+  DateTime get toDateTime {
+    if(calendarType == CalendarType.jalali){
+      return toJalali.toDateTime();
+    }
+    return DateTime(year, month, day);
+  }
+
+  int isAfter(CalendarDateTime dateTime) {
+    if (year > dateTime.year) {
       return 1;
     }
-    else if(year < dateTime.year){
+    else if (year < dateTime.year) {
       return -1;
     }
-    else{
-      if(month > dateTime.month){
+    else {
+      if (month > dateTime.month) {
         return 1;
       }
-      else if(month < dateTime.month){
+      else if (month < dateTime.month) {
         return -1;
       }
-      else{
-        if(day > dateTime.day){
+      else {
+        if (day > dateTime.day) {
           return 1;
         }
-        else if(day < dateTime.day){
+        else if (day < dateTime.day) {
           return -1;
         }
         return 0;
