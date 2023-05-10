d09ce0ba9114eac0a5a93f2efe60899fee9b1199
Mohsen Jalali <mniroomandj@gmail.com>
2023-05-10 15:38:46 +0330

    refactor: add menuSelection header

--- lib/src/widgets/calendar_header.dart
+++ lib/src/widgets/calendar_header.dart
@@ -7,73 +7,112 @@ class CalendarHeader extends StatelessWidget {
   final VoidCallback onPressNext;
   final VoidCallback onPressPrevious;
   final VoidCallback? onPressCurrentDate;
+  final VoidCallback? onPressMonth;
+  final VoidCallback? onPressYear;
   final HeaderModel? headerModel;
+  final bool isSelectionMenu;
+  final VoidCallback onPressBackOnMenu;
 
   const CalendarHeader({
     Key? key,
     required this.calendarDateTime,
     required this.onPressNext,
     required this.onPressPrevious,
+    required this.onPressBackOnMenu,
     this.headerModel = const HeaderModel(),
     this.onPressCurrentDate,
+    this.isSelectionMenu = false,
+    this.onPressMonth,
+    this.onPressYear,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    print(headerModel?.iconAlignment);
-    print(headerModel?.iconAlignment);
-    print(headerModel?.iconAlignment);
-    print(headerModel?.iconAlignment == HeaderIconAlignment.center);
     return Container(
       padding: headerModel?.padding,
       margin: headerModel?.margin,
       decoration: headerModel?.headerDecoration,
-      child: Row(
-        children: [
-          if(headerModel?.iconAlignment == HeaderIconAlignment.right) titleWidget(context),
+      child: Visibility(
+        visible: isSelectionMenu == false,
+        replacement: Row(
+          children: [
+            IconButton(
+              onPressed: onPressBackOnMenu,
+              icon: const Icon(Icons.arrow_back),
+              color: headerModel?.iconsColor ?? Colors.black,
+              splashRadius: 20,
+            ),
+          ],
+        ),
+        child: Row(
+          children: [
+            if (headerModel?.iconAlignment == HeaderIconAlignment.right)
+              titleWidget(context),
             IconButton(
               onPressed: onPressPrevious,
               icon: const Icon(Icons.arrow_back),
               color: headerModel?.iconsColor ?? Colors.black,
               splashRadius: 20,
             ),
-          if(headerModel?.iconAlignment == HeaderIconAlignment.center) titleWidget(context),
-          IconButton(
-            onPressed: onPressNext,
-            splashRadius: 20,
-            icon: Icon(
-              Icons.arrow_forward,
-              color: headerModel?.iconsColor ?? Colors.black,
+            if (headerModel?.iconAlignment == HeaderIconAlignment.center)
+              titleWidget(context),
+            IconButton(
+              onPressed: onPressNext,
+              splashRadius: 20,
+              icon: Icon(
+                Icons.arrow_forward,
+                color: headerModel?.iconsColor ?? Colors.black,
+              ),
             ),
-          ),
-          if(headerModel?.iconAlignment == HeaderIconAlignment.left) titleWidget(context),
-        ],
+            if (headerModel?.iconAlignment == HeaderIconAlignment.left)
+              titleWidget(context),
+          ],
+        ),
       ),
     );
   }
-
 
   Widget titleWidget(BuildContext context) {
     return Expanded(
       child: Row(
         children: [
           Expanded(
-            child: Text(
-              "${CalendarUtils.getMonthName(
-                  calendarDateTime, context)} ${calendarDateTime.year}",
-              style: headerModel?.titleStyle ??
-                  const TextStyle(
-                    fontSize: 18,
+            child: headerModel?.titleBuilder != null
+                ? headerModel!.titleBuilder!
+                    .call(calendarDateTime, onPressYear!, onPressMonth!)
+                : Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      GestureDetector(
+                        onTap: onPressMonth,
+                        child: Text(
+                          CalendarUtils.getMonthName(calendarDateTime, context),
+                          style: headerModel?.titleStyle ??
+                              const TextStyle(fontSize: 18),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 4,
+                      ),
+                      GestureDetector(
+                        onTap: onPressYear,
+                        child: Text(
+                          "${calendarDateTime.year}",
+                          style: headerModel?.titleStyle ??
+                              const TextStyle(fontSize: 18),
+                        ),
+                      ),
+                    ],
                   ),
-            ),
           ),
           Visibility(
-            visible: onPressCurrentDate != null,
+            visible: headerModel?.hasTodayIcon ?? false,
             child: IconButton(
               onPressed: onPressCurrentDate,
-              icon: const Icon(
+              icon: Icon(
                 Icons.today,
-                color: Colors.black,
+                color: headerModel?.iconsColor,
+                size: headerModel?.iconsSize,
               ),
             ),
           )
