c1c3030fbac224a0d144015076201aa3a1b1bb83
Mohsen Jalali <mniroomandj@gmail.com>
2023-05-10 16:05:26 +0330

    feat: add menu props

--- lib/src/model/custom_header_model.dart
+++ lib/src/model/custom_header_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_custom_calendar/flutter_custom_calendar.dart';
 
 enum HeaderIconAlignment { right, left, center }
 
@@ -7,9 +8,19 @@ class HeaderModel {
   final Color? iconsColor;
   final double? iconsSize;
   final Decoration? headerDecoration;
+  final TextStyle? menuItemStyle;
+  final TextStyle? selectedMenuItemStyle;
+  final Decoration? menuItemDecoration;
+  final Decoration? selectedMenuItemDecoration;
   final EdgeInsets? padding;
   final EdgeInsets? margin;
   final HeaderIconAlignment iconAlignment;
+  final bool hasTodayIcon;
+  final Widget Function(
+    CalendarDateTime calendarDateTime,
+    VoidCallback onSelectYear,
+    VoidCallback onSelectMont,
+  )? titleBuilder;
 
   const HeaderModel({
     this.titleStyle,
@@ -18,6 +29,12 @@ class HeaderModel {
     this.headerDecoration,
     this.padding,
     this.margin,
+    this.titleBuilder,
+    this.menuItemStyle,
+    this.selectedMenuItemStyle,
+    this.menuItemDecoration,
+    this.selectedMenuItemDecoration,
+    this.hasTodayIcon = true,
     this.iconAlignment = HeaderIconAlignment.right,
   });
 }
